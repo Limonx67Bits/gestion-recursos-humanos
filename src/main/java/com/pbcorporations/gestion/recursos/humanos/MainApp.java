445037e25c6bdb77a493be26ac59353d86ade95c
Limonx67Bits <lemmon2octv@gmail.com>
2026-09-14 11:02:18 -0600

    Primer commit

---
+++ src/main/java/com/pbcorporations/gestion/recursos/humanos/MainApp.java
@@ -0,0 +1,9 @@
+package main.java.com.pbcorporations.gestion.recursos.humanos;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
