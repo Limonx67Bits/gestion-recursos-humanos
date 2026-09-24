087c81539ba2e78694209d4eb401b2bcc21455ca
Judas <dbarrientos-2026360@kinal.edu.gt>
2026-09-23 22:08:36 -0600

    Reset database connection credentials
    Cleared database connection credentials.

--- src/main/java/com/pbcorporations/gestion/recursos/humanos/config/Credentials.java
+++ src/main/java/com/pbcorporations/gestion/recursos/humanos/config/Credentials.java
@@ -1,7 +1,7 @@
 package main.java.com.pbcorporations.gestion.recursos.humanos.config;
 
 public class Credentials {
-   public static final String URL = "jdbc:mysql://localhost:3306/gestion_rrhh_db_in4bv";
-    public static final String USER = "IN4BV";
-    public static final String PASSWORD = "%IndiVA4";
+   public static final String URL = "";
+    public static final String USER = "";
+    public static final String PASSWORD = "";
 }
